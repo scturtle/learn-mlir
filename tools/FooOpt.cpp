d4c011ff866bed582691ccd4c7036a87e3dcda1c
scturtle <scturtle@gmail.com>
2024-02-08 18:02:01 +0800

    llvm

--- tools/FooOpt.cpp
+++ tools/FooOpt.cpp
@@ -8,6 +8,52 @@
 #include "Transform/Affine/AffineFullUnroll.h"
 #include "Transform/Arith/Passes.h"
 
+#include "mlir/Pass/PassManager.h"
+
+void polyToLLVMPipelineBuilder(mlir::OpPassManager &manager) {
+  manager.addPass(mlir::foo::poly::createPolyToStandard());
+  manager.addPass(mlir::createCanonicalizerPass());
+
+  // arith op on tensor to linalg
+  manager.addPass(mlir::createConvertElementwiseToLinalgPass());
+  // tensor.pad
+  manager.addPass(mlir::createConvertTensorToLinalgPass());
+
+  // One-shot bufferize
+  // https://mlir.llvm.org/docs/Bufferization/#ownership-based-buffer-deallocation
+  mlir::bufferization::OneShotBufferizationOptions bufferizationOptions;
+  bufferizationOptions.bufferizeFunctionBoundaries = true;
+  manager.addPass(
+      mlir::bufferization::createOneShotBufferizePass(bufferizationOptions));
+  mlir::bufferization::BufferDeallocationPipelineOptions deallocationOptions;
+  mlir::bufferization::buildBufferDeallocationPipeline(manager,
+                                                       deallocationOptions);
+
+  // memref.subview
+  manager.addPass(mlir::memref::createExpandStridedMetadataPass());
+
+  // linalg to scf
+  manager.addPass(mlir::createConvertLinalgToLoopsPass());
+  // scf to llvm
+  manager.addPass(mlir::createConvertSCFToCFPass());
+  manager.addPass(mlir::createConvertControlFlowToLLVMPass());
+  // arith to llvm
+  manager.addPass(mlir::createArithToLLVMConversionPass());
+
+  // cannot be early or later
+  manager.addPass(mlir::createConvertFuncToLLVMPass());
+
+  // builtin.unrealized_conversion_cast
+  manager.addPass(mlir::createFinalizeMemRefToLLVMConversionPass());
+  manager.addPass(mlir::createReconcileUnrealizedCastsPass());
+
+  // cleanup
+  manager.addPass(mlir::createCanonicalizerPass());
+  manager.addPass(mlir::createSCCPPass());
+  manager.addPass(mlir::createCSEPass());
+  manager.addPass(mlir::createSymbolDCEPass());
+}
+
 int main(int argc, char **argv) {
   mlir::DialectRegistry registry;
   mlir::registerAllDialects(registry);
@@ -20,6 +66,10 @@ int main(int argc, char **argv) {
   mlir::foo::registerArithPasses();
 
   mlir::foo::poly::registerPolyToStandardPasses();
+
+  mlir::PassPipelineRegistration<>(
+      "poly-to-llvm", "Run passes to lower the poly dialect to LLVM",
+      polyToLLVMPipelineBuilder);
 
   return mlir::asMainReturnCode(
       mlir::MlirOptMain(argc, argv, "foo opt\n", registry));
